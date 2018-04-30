ee6e71146215df8ace9d665d3e5df5779cd9f97e
musab <musab.erayman@ug.bilkent.edu.tr>
2018-04-30 17:36:27 +0300

    app.c cmmit

--- app.c
+++ app.c
@@ -2,18 +2,13 @@
 #include <sys/types.h>
 #include <unistd.h>
 int recursiveFunc(int n){
-	if (n == 0 || n == 1){
-		int a = 0;
-		return n;
-	}
-	else{
-		int result = recursiveFunc(n-1) + recursiveFunc(n-2);
-		if(result > 6700){
-			printf("%s\n","check the stack size then enter 0 to return");
-			int a = 0;
-			scanf("%d",&a);
-		}
-	}
+	int intt[1000];
+	printf("Stack at %p\n", (void*)(&intt));
+	if(n>0)
+		recursiveFunc(n-1);
+	else
+		return;
+	
 }
 int main() {
    int size;
@@ -23,10 +18,10 @@ int main() {
    printf("pid= %d\n",pid);
 
    while(1){
-	   printf( "Enter a value to allocate enter 0 to terminate -1 to recursive call: \n");
+	   printf( "Enter a value to allocate, enter 0 to terminate, -1 to recursive call: \n");
 	   scanf("%d",&size);
 	   if(size <= -1){
-	   		int checkpoint = 20;
+	   		int checkpoint = 100;
 	   		recursiveFunc(checkpoint);
 	   }
 	   else if(size >0){
